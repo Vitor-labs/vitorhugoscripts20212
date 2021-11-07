5b62203daaa5275bc2c88f29377a46b49fa4ae9a
Vitor Hugo <v02hx10@gmail.com>
2021-11-06 23:39:35 -0300

    on test...

--- Atividades/atividade03/logins.sh
+++ Atividades/atividade03/logins.sh
@@ -1,8 +1,5 @@
 # Correção: 2,0
 grep -v 'sshd' auth.log
-
 egrep 'sshd:session): session opened for user j.' auth.log
-
 egrep -c 'root' auth.log
-
 egrep -c '^(Oct 1(1|2)|).{55}session opened' auth.log
