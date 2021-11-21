c9c809248ebeead6db41a2d17c2fbc6d82a74e94
Vitor Hugo <v02hx10@gmail.com>
2021-11-20 23:36:47 -0300

    atividade 7 - testando

--- Atividades/atividade07/alertaDiretorio.sh
+++ Atividades/atividade07/alertaDiretorio.sh
@@ -7,24 +7,19 @@ path=${2}
 #variaveis de log
 log_file="dirSensors.log"
 temp=$(date +"%F %H:%M:%S")
-count_files=0
-for file in $path; do
-    if [ -f $file ]; then
-        count_files=$((count_files+1))
-    fi
-done
+count_files=$(ls -l $path | wc -l)
 
 #verifica se o diretorio existe
 if [ -d ${path} ]; then
     echo "[$temp]: Diretorio ${path} existe" >> ${log_file}
 else
     echo "[$temp]: Diretorio ${path} nao existe" >> ${log_file}
-    exit 0
+    exit
 fi
 #verifica se o diretorio esta vazio
 if [ -z "$(ls -A ${path})" ]; then
     echo "[$temp]: Diretorio ${path} esta vazio" >> ${log_file}
-    exit 0
+    exit
 else
     echo "[$temp]: Diretorio ${path} nao esta vazio" >> ${log_file}
 fi
@@ -47,12 +42,12 @@ while true; do
         fi
     done
     #verifica se o numero de arquivos no diretorio é menor que o contador
-    if [ $counter > $count_files ]; then
+    if [ $counter -gt $count_files ]; then
         echo "[$temp] Alteração! $count_files->$counter. Removidos: $file" >> ${log_file}
         echo "[$temp]: Arquivo ${file} foi removido" >> ${log_file}
     fi
     #verifica se o numero de arquivos no diretorio é maior que o contador
-    if [ $counter < $count_files ]; then
+    if [ $count_files -gt $counter ]; then
         echo "[$temp] Alteração! $count_files->$counter. Adicionados: $file" >> ${log_file}
         echo "[$temp]: Arquivo ${file} foi adicionado" >> ${log_file}
     fi
